4d90ef64d518dd16cd8f612ce5f2b351ffde74f5
Manish_Syal <manishsyal77@gmail.com>
2025-03-03 17:27:19 +0530

    enabled Cross origin request from anywhere

--- src/main/java/com/aiemail/writer/app/EmailGeneratorController.java
+++ src/main/java/com/aiemail/writer/app/EmailGeneratorController.java
@@ -2,12 +2,11 @@ package com.aiemail.writer.app;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*") //from any origin
+//@CrossOrigin("http://localhost:5173/")
 @RequestMapping("/api/email")
 @AllArgsConstructor
 public class EmailGeneratorController {
--- src/main/java/com/aiemail/writer/app/EmailGeneratorService.java
+++ src/main/java/com/aiemail/writer/app/EmailGeneratorService.java
@@ -75,11 +75,12 @@ public class EmailGeneratorService {
 
     private String buildPrompt(EmailRequest emailRequest) {
         StringBuilder prompt=new StringBuilder();
-        prompt.append("Generate a professional email reply for the following email content. Please don't generate subject line ");
+        prompt.append("Generate a professional email reply for the following email content. Please don't generate the subject line, greeting, or closing.");
         if(emailRequest.getTone() !=null && !emailRequest.getTone().isEmpty()){
             prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
         }
         prompt.append("\nOriginal email: \n").append(emailRequest.getEmailContent());
+        prompt.append(" Generate only one body reply text.");
         return prompt.toString();
     }
 }
